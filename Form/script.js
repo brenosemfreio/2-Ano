9eec2280e82d121bb54534fd08ccbea604759755
dantas <160528218+brenosemfreio@users.noreply.github.com>
2025-03-30 18:58:52 -0300

    Add files via upload

---
+++ Form/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        var usuario = document.getElementById("usuario").value;
+        var senha = document.getElementById("senha").value;
+        var mensagem = document.getElementById("mensagem");
+        
+        if (usuario === "admin" && senha === "1234") {
+            mensagem.textContent = "Login bem-sucedido!";
+            mensagem.style.color = "green";
+        } else {
+
+            mensagem.textContent = "Usuário ou senha incorretos!";
+            mensagem.style.color = "red";
+        }
+    });
+});
